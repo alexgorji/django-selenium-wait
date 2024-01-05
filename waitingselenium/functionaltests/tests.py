ab72affc50e0ae73514fd7e1103c236428b4bd7f
alexgorji <aligorji@hotmail.com>
2024-01-05 13:17:18 +0100

    page with 500000 lines

--- waitingselenium/functionaltests/tests.py
+++ waitingselenium/functionaltests/tests.py
@@ -8,6 +8,6 @@ from functionaltests.base import FunctionalTest
 class TestWait(FunctionalTest):
     def test_scroll_down_and_wait(self):
         self.driver.get(f"{self.live_server_url}{reverse('demo')}")
-        # self.driver.maximize_window()
-        # self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        # self.driver.find_element(By.ID, 'id_demo_link').click()
+        self.driver.maximize_window()
+        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.find_element(By.CSS_SELECTOR, "input[value='Save']").click()
